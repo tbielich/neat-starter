dbc4b56e777c6e6cf82bc9addb5f76f850c566f6
Thomas Bielich <t.bielich@konsumkit.com>
2020-12-18 20:12:29 +0100

    add fahrzeuge

--- .eleventy.js
+++ .eleventy.js
@@ -39,6 +39,7 @@ module.exports = function (eleventyConfig) {
 
   // Copy Image Folder to /_site
   eleventyConfig.addPassthroughCopy("./src/static/img");
+  eleventyConfig.addPassthroughCopy("./fahrzeuge");
 
   // Let Eleventy transform HTML files as nunjucks
   // So that we can use .html instead of .njk
